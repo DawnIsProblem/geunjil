2bb2abd0916d120064f32157fd47c8736c190006
DawnIsProblem <qweqwerty12321@gmail.com>
2025-08-04 18:03:49 +0900

    feat: Add redis config

---
+++ backend/src/main/java/com/geunjil/geunjil/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.geunjil.geunjil.config;
+
+import com.geunjil.geunjil.domain.challenge.dto.request.ChallengeUserLocationDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, ChallengeUserLocationDto> userLocationRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, ChallengeUserLocationDto> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
